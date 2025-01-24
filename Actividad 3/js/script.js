c672707e6fc70600e08191727c0b14387b9cdd55
agarsan1903 <agarsan1903@g.educaand.es>
2025-01-24 11:38:44 +0100

    Add files via upload

---
+++ Actividad 3/js/script.js
@@ -0,0 +1,7 @@
+function pedirdatos() {
+    nombre=prompt("¿Como te llamas?");
+    edad=prompt("¿Que edad tienes?");
+    alert("Hola soy " + nombre + " y tengo " + edad + " años ");
+    let=nombre
+    let=edad
+}
