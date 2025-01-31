4cb494f8769bc6a9e57550db3774b378b7162b10
agarsan1903 <agarsan1903@g.educaand.es>
2025-01-31 11:12:06 +0100

    lo he puesto antes ya

--- Actividad 3/js/script.js
+++ Actividad 3/js/script.js
@@ -1,7 +1,7 @@
 function pedirdatos() {
+    let=nombre
+    let=edad
     nombre=prompt("¿Como te llamas?");
     edad=prompt("¿Que edad tienes?");
     alert("Hola soy " + nombre + " y tengo " + edad + " años ");
-    let=nombre
-    let=edad
 }
