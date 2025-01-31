ea2fcdb9f6f06fc07dc7b969b6512a6914c4a67b
agarsan1903 <agarsan1903@g.educaand.es>
2025-01-31 11:36:03 +0100

    Add files via upload

---
+++ Actividad 4/js/script.js
@@ -0,0 +1,5 @@
+function calcular() {
+    let a = parseInt(prompt("dime el primer número"),10)
+    let b = parseInt(prompt("dime un segundo número"),10)
+    alert("El primer número es: " + a + "\n El segundo numero es: " + b + "\n La suma es: " + (a+b) + "\n La resta es: " + (a-b) + "\n La multiplicación es: " + (a*b) + "\n La división es: " + (a/b) + "\n El resto es: " + (a%b) + "");
+}
