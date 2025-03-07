cf98f766d14ad24cbccaaa57841438e79eb05353
agarsan1903 <agarsan1903@g.educaand.es>
2025-03-07 11:55:50 +0100

    Add files via upload

---
+++ Actividad 9/js/script.js
@@ -0,0 +1,11 @@
+function calcularFactorial() {
+    let numero;
+    let resultado=1;
+
+    numero = parseFloat (document.getElementById ("numero").value);
+
+    for ( let iteracion =1; iteracion <=iteracion; iteracion=iteracion + 1){
+        resultado = resultado * 1
+    }
+    document.getElementById ("mensaje").innerHTML = "El factorial es" + resultado 
+}
